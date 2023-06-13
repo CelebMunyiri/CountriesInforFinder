e8cdb323d77e69adf7c0a0afcdb4ce96936da9a0
David Munyiri <davidmumyiri2019@gmail.com>
2023-06-13 14:20:04 +0300

    implemented neighbouring country

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
+const btn=document.querySelector('.btn-country');
+const countriesContainer=document.querySelector('.countries');
+
 //Async js are used to make ajax calls to API's
 /*
 const p=document.querySelector(".p");
@@ -13,7 +16,7 @@ setTimeout(function(){
 p.style.color='red';
 //asynchrnous is when actions do not occur at the same time 
 //Ajax=> asynchronous js and XML
-//API=> Application Programming Interface*/
+//API=> Application Programming Interface
 //Making first ajax and API call
 const btn=document.querySelector('.btn-country');
 const countriesContainer=document.querySelector('.countries');
@@ -42,9 +45,74 @@ request.addEventListener('load',function(){
 
         countriesContainer.insertAdjacentHTML('beforeend',html)
         countriesContainer.style.opacity=1;
-})
+}) 
 };
 const info=prompt('Enter a country name to see its data')
 getCountryData(info)
 //getCountryData('usa')
-//getCountryData('kenya')
+//getCountryData('kenya')*/
+
+const renderCountry=function(data,className=''){
+
+  const html=`
+      <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+              <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+          </article>`;
+  
+           countriesContainer.insertAdjacentHTML('beforeend',html)
+          countriesContainer.style.opacity=1;
+}
+
+const getCountryAndNeighbor =function(country){
+  //Ajax call country 1
+  const request=new XMLHttpRequest();
+  request.open('GET',`https://restcountries.com/v2/name/${country}`)
+  request.send();
+  
+  request.addEventListener('load',function(){
+      console.log(this.responseText)
+      const [data]=JSON.parse(this.responseText);
+      console.log(data);
+  
+//render country 1
+      renderCountry(data);
+
+      //Get neighbour country
+      const [neighbour]=data.borders;
+      
+      if(!neighbour) return;
+
+      //AJAX call country 2
+      const request2=new XMLHttpRequest();
+  request2.open('GET',`https://restcountries.com/v2/alpha/${neighbour}`)
+  request2.send();
+  request2.addEventListener('load',function(){
+    //console.log(this.responseText);
+    const data2=JSON.parse(this.responseText)
+    //console.log(data2)
+    renderCountry(data2,'neighbour')
+  })
+  }) 
+}
+
+//Callback hell
+getCountryAndNeighbor('somalia');
+setTimeout(()=>{
+  console.log('1 second passed')
+  setTimeout(()=>{
+    console.log('2 second passed')
+    setTimeout(()=>{
+      console.log('3 second passed')
+      setTimeout(()=>{
+        console.log('4 second passed')
+      },1000)
+    },1000)
+  },1000)
+},1000)
